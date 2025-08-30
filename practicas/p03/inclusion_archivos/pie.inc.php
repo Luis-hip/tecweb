5d8de8f784e741f732289f3f9b7393152ede1177
luiso <oh202341121@alm.buap.mx>
2025-08-29 20:41:10 -0600

    código pie.inc.php agregado

---
+++ practicas/p03/inclusion_archivos/pie.inc.php
@@ -0,0 +1,9 @@
+<hr />
+    <?php
+        echo "<div><h1 style=\"border-width:5;border-style:double;background-color:#ffcc99;\">
+        Final de la página PHP Vínculos útilizados: <a href=\"php.net\">php.net</a> &nbsp; <a href=\"mysql.org\">mysql.org</a></h1>";
+        echo "Nombre de archivo ejecutado: ", $_SERVER['PHP_SELF'], "&nbsp;&nbsp;&nbsp;";
+        echo "Nombre del archivo incluido: ", __FILE__, "</div>";
+    ?>
+    </body>
+</html>
