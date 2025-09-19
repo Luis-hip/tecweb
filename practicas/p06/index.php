86e912a7ebd93533e3926ba5fc071d56a689838f
luiso <oh202341121@alm.buap.mx>
2025-09-18 21:42:41 -0600

    Ejercicio 6 actualizado

--- practicas/p06/index.php
+++ practicas/p06/index.php
@@ -2,7 +2,7 @@
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" 
     "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" lang="en">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="es">
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -12,7 +12,7 @@
     <h2>Ejercicio 1</h2>
     <p>Escribir programa para comprobar si un número es un múltiplo de 5 y 7</p>
     <?php 
-            $numero = $_GET['numero'];
+            @$numero = $_GET['numero'];
             echo esMultiploDe5y7($numero);
     ?>
 
@@ -62,7 +62,28 @@
 
     <h2>Ejercicio 6</h2>
     <p>Crear en codigo duro un arreglo asociativo que sirva para registrar el parque vehicular de una ciudad.</p>
-    <?php mostrarParqueVehicular($parqueVehicular); ?>
     
+    <h4>Consulta de Parque Vehicular</h4>
+    <form action="http://localhost/tecweb/practicas/p06/index.php" method="post">
+        <label for="matricula">Matrícula:</label>
+        <input type="text" name="matricula" id="matricula" placeholder="Ej. ABC1234">
+        <br><br>
+        <button type="submit" name="buscar">Buscar</button>
+        <button type="submit" name="mostrar_todos">Mostrar Todos</button>
+    </form>
+    <?php
+        if(isset($_POST['buscar']) && isset($_POST['matricula']) && $_POST['matricula'] != ""){
+            $matricula = strtoupper(trim($_POST['matricula']));
+            if(isset($parqueVehicular[$matricula])){
+                mostrarParqueVehicular([$matricula => $parqueVehicular[$matricula]]);
+            }
+            else{
+                echo "<p>No se encontró la matrícula <strong>$matricula</strong> en el parque vehicular.</p>";
+            }
+        }
+        elseif(isset($_POST['mostrar_todos'])){
+           mostrarParqueVehicular($parqueVehicular);
+        }
+    ?>
 </body>
 </html>
