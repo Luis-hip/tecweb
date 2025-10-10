a55bd0831b87eccac29ba793fc955e203a8ba4c0
luiso <oh202341121@alm.buap.mx>
2025-10-09 23:15:57 -0600

    Estilos en la tabla prodcutos vigentes

--- practicas/p08/get_productos_vigentes.php
+++ practicas/p08/get_productos_vigentes.php
@@ -22,13 +22,14 @@
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     <title>Productos Vigentes</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="styles.css" / >
 </head>
 <body>
-    <div class="container">
+    <div class="container-large">
         <h1>Productos Vigentes</h1>
         <?php if (isset($rows) && count($rows) > 0) : ?>
             <table class="table">
-                <thead>
+                <thead class="thead-dark">
                     <tr>
                         <th>#</th>
                         <th>Nombre</th>
