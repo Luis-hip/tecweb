ecae3ce11747ca29aeb19170212d72b9e2ec6907
luiso <oh202341121@alm.buap.mx>
2025-10-15 21:33:49 -0600

    read.php midificado con busqueda por texto

--- practicas/p10/product_app/backend/read.php
+++ practicas/p10/product_app/backend/read.php
@@ -3,8 +3,26 @@
 
     // SE CREA EL ARREGLO QUE SE VA A DEVOLVER EN FORMA DE JSON
     $data = array();
-    // SE VERIFICA HABER RECIBIDO EL ID
-    if( isset($_POST['id']) ) {
+    
+    //Busqueda por texto
+    if(isset($_POST['search'])){ //Verificamos haber recibido un parámetro de búsqueda
+        $search = $_POST['search'];
+        $sql = "SELECT * FROM productos WHERE nombre LIKE '%$search%' OR marca LIKE '%$search%' OR detalles LIKE '%$search%' AND eliminado = 0";
+        if($result = $conexion->query($sql)){
+            // SE OBTIENEN LOS RESULTADOS y se mapenan 
+            while($row = $result->fetch_array(MYSQLI_ASSOC)){
+                $data[] = $row;
+            }
+            $result->free();
+        }
+        else {
+            die('Query Error: '.mysqli_error($conexion));
+        }
+        $conexion->close();
+    }
+
+    //Busqueda por ID 
+    /*if( isset($_POST['id']) ) { // SE VERIFICA HABER RECIBIDO EL ID
         $id = $_POST['id'];
         // SE REALIZA LA QUERY DE BÚSQUEDA Y AL MISMO TIEMPO SE VALIDA SI HUBO RESULTADOS
         if ( $result = $conexion->query("SELECT * FROM productos WHERE id = '{$id}'") ) {
@@ -22,8 +40,9 @@
             die('Query Error: '.mysqli_error($conexion));
         }
 		$conexion->close();
-    } 
+    } */
     
     // SE HACE LA CONVERSIÓN DE ARRAY A JSON
     echo json_encode($data, JSON_PRETTY_PRINT);
 ?>
+
