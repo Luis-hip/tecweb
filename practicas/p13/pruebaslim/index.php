16bbafc3d9cda471a7101ba438ca39b5550ffee4
DiegoRdgzz <rt202354027@alm.buap.mx>
2025-11-23 05:11:01 -0600

    paso 3 practica 13 completado

--- practicas/p13/pruebaslim/index.php
+++ practicas/p13/pruebaslim/index.php
@@ -16,5 +16,13 @@ $app->get("/hola[/{nombre}]", function( $request, $response, $args ){
     return $response;
 });
 
+//Ejemplo 3
+$app->post("/pruebapost", function( $request, $response, $args ){
+    $reqPost = $request->getParsedBody();
+    $val1 = $reqPost["val1"] ?? '';
+    $val2 = $reqPost["val2"] ?? '';
 
+    $response->getBody()->write("Valores: " . $val1 . " " . $val2);
+    return $response;
+});
 $app->run();
